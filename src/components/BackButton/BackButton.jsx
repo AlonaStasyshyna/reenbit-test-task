6f78195783c50a1d5f738237e81883fb148ea729
AlonaStasyshyna <alena.stasishyna28@gmail.com>
2023-03-16 01:18:46 +0200

    Add navigation to the button

--- src/components/BackButton/BackButton.jsx
+++ src/components/BackButton/BackButton.jsx
@@ -1,7 +1,17 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+
 export const BackButton = () => {
+  const location = useLocation();
+  const navigation = useNavigate();
+
   return (
     <header>
-      <button>GO BACK</button>
+      <button
+        type="button"
+        onClick={() => navigation(location?.state?.from || '/characters')}
+      >
+        GO BACK
+      </button>
     </header>
   );
 };
